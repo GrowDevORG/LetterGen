999279ca99f0ef72a076ade34688b434c15f164c
Vikas <101702902+NotYourEren@users.noreply.github.com>
2025-02-19 11:17:29 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,18 @@ import Footer from '@/components/Footer';
 import HeroSection from '@/pages/HeroSection';
 import HowItWork from '@/pages/HowItWork';
 import InfoPage from '@/pages/InfoPage';
-import { useSession, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
   const { data: session, status } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+  // if (session) {
+  //   return (
+  //     <>
+  //       Signed in as {session.user.email} <br />
+  //       <button onClick={() => signOut()}>Sign out</button>
+  //     </>
+  //   );
+  // }
   return (
     <main className="flex flex-col bg-gray-100">
       {/* Not signed in <br />
