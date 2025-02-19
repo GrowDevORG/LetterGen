7aec30bbc63a2ce3c09b4837970f360780da330a
Vikas <101702902+NotYourEren@users.noreply.github.com>
2025-02-19 19:38:19 +0530

    added custom auth page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+import AuthPage from '@/pages/AuthPage';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <AuthPage />
+    </div>
+  );
+};
+
+export default page;
